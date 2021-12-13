1f1346843b6fa9cba0928643ec73a39d7087bc4e
DArmstrong87 <lenzflare@gmail.com>
2021-12-13 10:09:56 -0600

    saving auth viewset

--- kineticapi/views/auth.py
+++ kineticapi/views/auth.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from kineticapi.models import Athlete, Organizer, athlete, organizer
+from kineticapi.models import Athlete, Organizer
 from rest_framework import status
 
 
